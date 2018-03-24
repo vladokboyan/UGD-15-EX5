968432852aff418daee106338322ef2c799e38fd
Billy Everyteen <vasa>
2018-03-24 14:44:21 +0200

    EXFIVE

---
+++ UGD-15-EX4-master/Assets/skripts/PlayerContloreller.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerContloreller : MonoBehaviour
+{
+
+    public float speed = 10.0f;
+    public float gravity = -9.8f;
+    public float jumpheight = 3.0f;
+
+    private CharacterController _charCont;
+
+    void Start()
+    {
+        _charCont = GetComponent<CharacterController>();
+    }
+
+    // Use this for initialization
+    void Update()
+    {
+        float deltaX = Input.GetAxis("Horizontal") * speed;
+        float deltaZ = Input.GetAxis("Vertical") * speed;
+        Vector3 movement = new Vector3(deltaX, 0, deltaZ);
+        movement = Vector3.ClampMagnitude(movement, speed);
+
+        movement.y = gravity;
+
+        movement *= Time.deltaTime;
+        movement = transform.TransformDirection(movement);
+        _charCont.Move(movement);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Vector3 jump = new Vector3(0, jumpheight, 0);
+            jump = Vector3.ClampMagnitude(jump, jumpheight);
+            _charCont.Move(jump);
+        }
+    }
+}
---
+++ UGD-15-EX4-master/Assets/skripts/camerecontlorel.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camerecontlorel : MonoBehaviour {
+
+    public enum AxisRotation
+    {
+        mouseX = 1,
+        mouseY = 2
+    }
+
+    public AxisRotation axes = AxisRotation.mouseX;
+
+    public float minimumVert = -45.0f;
+    public float maximumVert = 45.0f;
+
+    public float sHorizontal = 10.0f;
+    public float sVertical = 10.0f;
+
+    public float _rotationX = 0;
+
+    // Update is called once per frame
+    void Update ()
+    {
+		if(axes == AxisRotation.mouseX)
+        {
+            transform.Rotate(0, Input.GetAxis("Mouse X") * sHorizontal, 0);
+        }
+        else if (axes == AxisRotation.mouseY)
+        {
+            _rotationX -= Input.GetAxis("Mouse Y") * sVertical;
+            _rotationX = Mathf.Clamp(_rotationX, minimumVert, maximumVert);
+
+            float _rotationY = transform.localEulerAngles.y;
+
+            transform.localEulerAngles = new Vector3(_rotationX, _rotationY,0);
+        }
+	}
+}
